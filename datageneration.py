013546424a19d9a5f1a6eef88de06a108034476b
sandeepbanisetti21 <31123174+sandeepbanisetti21@users.noreply.github.com>
2018-02-12 23:48:42 -0800

    Updated

--- datageneration.py
+++ datageneration.py
@@ -2,18 +2,18 @@ from random import randint, choice
 import datetime
 import string
 
-users = 10
-friends = int(2.5*users)
-posts = int(1.4*users)
-comments = int(10*posts)
+USERS = 10
+friends = int(2.5*USERS)
+POSTS = int(1.4*USERS)
+COMMENTS = int(10*POSTS)
 
 
 
-def get_gender():
-    gender = ["M", "F"]
-    return gender[randint(0,1)]
+def get_GENDER():
+    GENDER = ["M", "F"]
+    return GENDER[randint(0,1)]
 
-def get_text():
+def get_TEXT():
     string_domain = string.ascii_letters + string.digits
     return "".join(choice(string_domain) for _ in range(0, randint(10, 20)))
 
@@ -22,43 +22,43 @@ def get_time():
     date = datetime.date(randint(2005, 2025), randint(1, 12), randint(1, 28))
     return "{}-{}-{}".format(date.year, date.month, date.day)
 
-def get_users():
+def get_USERS():
     data = ""
-    for i in range(0, users):
-        data += "INSERT INTO Users (name, gender, date_of_birth) values " \
-                "(\"{}\", \"{}\", \"{}\");".format(get_text(), get_gender(), get_time())
+    for i in range(0, USERS):
+        data += "INSERT INTO USERS (NAME, GENDER, DATE_OF_BIRTH) values " \
+                "(\"{}\", \"{}\", \"{}\");".format(get_TEXT(), get_GENDER(), get_time())
         data += "\n"
     return data
 
-def get_friendships():
+def get_FRIENDSHIPS():
     data = ""
     for i in range(0, friends):
-        u1 = randint(1, users)
-        u2 = randint(1, users)
-        data += "INSERT INTO Friendships (user_id, friend_id) values ({},{});".format(u1, u2)
+        u1 = randint(1, USERS)
+        u2 = randint(1, USERS)
+        data += "INSERT INTO FRIENDSHIPS (USER_ID, FRIEND_ID) values ({},{});".format(u1, u2)
         data += "\n"
     return data
 
-def get_posts():
+def get_POSTS():
     data = ""
-    for i in range(0, posts):
-        u = randint(1, users)
-        data += "INSERT INTO Posts (user_id, text) values ({}, \"{}\");".format(u, get_text())
+    for i in range(0, POSTS):
+        u = randint(1, USERS)
+        data += "INSERT INTO POSTS (USER_ID, TEXT) values ({}, \"{}\");".format(u, get_TEXT())
         data += "\n"
     return data
 
-def get_comments():
+def get_COMMENTS():
     data = ""
-    for i in range(0, comments):
-        u = randint(1, users)
-        p = randint(1, posts)
-        data += "INSERT INTO Comments (post_id, commenter_user_id, text) values ({}, {}, \"{}\");".format(p, u, get_text())
+    for i in range(0, COMMENTS):
+        u = randint(1, USERS)
+        p = randint(1, POSTS)
+        data += "INSERT INTO COMMENTS (POST_ID, COMMENTER_USER_ID, TEXT) values ({}, {}, \"{}\");".format(p, u, get_TEXT())
         data += "\n"
     return data
 
 
 def get_data():
-    data = [get_users(),  get_friendships(),  get_posts(), get_comments()]
+    data = [get_USERS(),  get_FRIENDSHIPS(),  get_POSTS(), get_COMMENTS()]
     with open("bootstrap.sql", 'w') as f:
         f.write("USE assignment2\n")
         for d in data:
